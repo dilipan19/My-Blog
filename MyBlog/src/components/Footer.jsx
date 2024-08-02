bf4c561bb33fc57f84eac2ed6682e236622b8482
dilipan19 <dilipangopal@gmail.com>
2024-08-02 10:12:02 +0530

    msg

---
+++ MyBlog/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <div className="mt-8  w-full bg-black px-8 md:px-[500px] flex justify-between text-sm md:text-md py-8">
+        
+    </div>
+  )
+}
+
+export default Footer
