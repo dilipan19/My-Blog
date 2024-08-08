4af1451941b287feb4a3a8e8c244706929999e6d
dilipan19 <dilipangopal@gmail.com>
2024-08-08 16:32:06 +0530

    updated1

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
         </h1>
         <h3><Link to="/register" className="text-white">Register</Link></h3> 
       </div> 
-      <div className="w-full flex justify-center items-center h-[70vh] bg-yellow-600"> 
+      <div className="w-full flex justify-center items-center h-[100vh] bg-yellow-600"> 
         <div className="flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%] bg-black p-6 rounded-lg shadow-lg"> 
           <h1 className="text-xl font-bold text-left text-white">Login</h1> 
           <input 
