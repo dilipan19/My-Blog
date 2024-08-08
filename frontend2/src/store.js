4620fb3d5d8cd565019178091c2bc1cd8fe8be59
dilipan19 <dilipangopal@gmail.com>
2024-08-08 16:17:57 +0530

    updated

--- frontend2/src/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers'; // Assuming you have rootReducer setup
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
