a5343a37694f6e9b4c53e9ecd9253378ef5b153d
dilipan19 <137703230+dilipan19@users.noreply.github.com>
2025-02-11 17:56:01 +0530

    Update server.js
    saa

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('
                                     Hello this is dilipan! and my student id is: M008729
-                                    i'll upskill myself by completing this course with practical knowledge
+                                    123
                                     '));
 
 app.listen(PORT, () => console.log(Server running on port ${PORT}));
